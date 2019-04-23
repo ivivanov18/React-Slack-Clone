808a93c2c4dcb9ff0e0c23ed4aa96ba884590626
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-04-23 23:31:58 +0300

    init firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+const config = {
+  apiKey: "AIzaSyDW2ddJ2PEgPGYnNAi4pLwVj23hQpdk2CQ",
+  authDomain: "react-slack-clone-4853d.firebaseapp.com",
+  databaseURL: "https://react-slack-clone-4853d.firebaseio.com",
+  projectId: "react-slack-clone-4853d",
+  storageBucket: "react-slack-clone-4853d.appspot.com",
+  messagingSenderId: "902007174855"
+};
+
+firebase.initializeApp(config);
