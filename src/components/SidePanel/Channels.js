0d17d14f2b55ababb6514a6a6f090317fb4c2e5b
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-05-06 14:25:07 +0300

    create channels collection, add addChannel function

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Menu, Icon, Modal, Form, Input, Button } from "semantic-ui-react";
+import firebase from "../../firebase";
 
-function Channels() {
+function Channels({ currentUser }) {
   const [channels, setChannels] = useState([]);
   const [modal, setModal] = useState(false);
   const [channelName, setChannelName] = useState("");
   const [channelDetails, setChannelDetails] = useState("");
+  const channelsRef = useRef(firebase.database().ref("channels"));
 
   const closeModal = () => {
     setModal(false);
@@ -15,13 +17,48 @@ function Channels() {
     setModal(true);
   };
 
-  const handleChannelName = evt => {
+  const handleChannelNameChange = evt => {
     setChannelName(evt.target.value);
   };
 
-  const handleChannelDetails = evt => {
+  const handleChannelDetailsChange = evt => {
     setChannelDetails(evt.target.value);
   };
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    if (isFormValid({ channelName, channelDetails })) {
+      addChannel();
+    }
+  };
+
+  const addChannel = () => {
+    console.log("in addChannel", channelsRef);
+    const key = channelsRef.current.push().key;
+    const newChannel = {
+      id: key,
+      name: channelName,
+      details: channelDetails,
+      createdBy: {
+        name: currentUser.displayName,
+        avatar: currentUser.photoURL
+      }
+    };
+
+    channelsRef.current
+      .child(key)
+      .update(newChannel)
+      .then(() => {
+        setChannelName("");
+        setChannelDetails("");
+        closeModal();
+        console.log("added channel");
+      })
+      .catch(err => console.error(err));
+  };
+
+  const isFormValid = ({ channelName, channelDetails }) =>
+    channelName && channelDetails;
 
   return (
     <React.Fragment>
@@ -42,7 +79,7 @@ function Channels() {
                 fluid
                 label="Name Of Channel"
                 name="channelName"
-                onChange={handleChannelName}
+                onChange={handleChannelNameChange}
               />
             </Form.Field>
             <Form.Field>
@@ -50,13 +87,13 @@ function Channels() {
                 fluid
                 label="About the Channel"
                 name="channelDetails"
-                onChange={handleChannelDetails}
+                onChange={handleChannelDetailsChange}
               />
             </Form.Field>
           </Form>
         </Modal.Content>
         <Modal.Actions>
-          <Button color="green" inverted>
+          <Button color="green" inverted onClick={handleSubmit}>
             <Icon name="checkmark" />
             Check
           </Button>
